51ec3827dc68b342dc684ce050cbeb6eaf7e25e1
Debora <deboravolc2@gmail.com>
2024-05-25 10:52:35 -0300

    adicionar modificacoes

--- js/variaveis.js
+++ js/variaveis.js
@@ -11,7 +11,7 @@ var objeto = {
     "altura":1.78,
     "situacao":true,
     "linguage_fav": ['Java', 'JS', 'PHP']
-}
+} //instanciar um objeto sem ter uma classe 
 
 var vazio = null;
 var indefinido = undefined;
@@ -20,4 +20,72 @@ var indefinido = undefined;
 console.log(num,txt,logico_v,
     logico_f,lista_mesmo_tipo,lista_tipo_mesclado,objeto,vazio, indefinido)
 
+//re-atribuicao de valores
+txt='Texto substituído'
+
+//re-atribuicao de dados
+logico_v='valores'
+
+console.log(txt,logico_v)
+
+//objeto por dentro do objeto ou objeto alinhado ou contido
+var aluno = {
+    "nome": "João",
+    "dt_nascimento": "25/05/2002",
+    "endereco": 
+        {"rua":"abc,123",
+        "cidade": "São Paulo",
+        "uf": "SP"
+    }
+}
+
 //escopo das variaveis
+//variaveis locais e globais
+
+var global="Eu sou global"
+console.log(global) //todo arquivo javascript irá enxergar esta variavel 
+
+//criando uma função
+function minha_funcao(){
+    //entre parenteses, colocamos os parâmetros de entrada da função
+    //esta variável está por dentro de um escopo
+    var local = "Eu sou local";
+    console.log(local)
+}
+
+
+//chamada de funcao - utilizada para executar a mesma na raiz
+minha_funcao()
+
+console.log(local,global)
+//nao é possível chamar uma variavel fora do escopo
+
+
+//let, var e const 
+function teste_var(){
+    var x = 10
+    if (x==10){
+        var x = 5;
+        console.log(x)
+    }
+    console.log(x)
+}
+
+function teste_let(){
+    let x = 10
+    if (x==10){
+        let x = 5;
+        console.log(x)
+    }
+    console.log(x)
+}
+
+function teste_const(){
+    const x = 10
+    if (x==10){
+        const x = 5;
+        console.log(x)
+    }
+    console.log(x)
+}
+
