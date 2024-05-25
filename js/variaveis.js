0afa8004fd9f7091fdc419288783e0337c6d1d66
Debora <deboravolc2@gmail.com>
2024-05-25 10:27:28 -0300

    incluir arquivos

---
+++ js/variaveis.js
@@ -0,0 +1,23 @@
+//tipos de variaveis
+var num =1;
+var txt = 'Olá'
+var logico_v = true;
+var logico_f = false;
+var lista_mesmo_tipo = ['Arroz', 'Feijao', 'Batata']
+var lista_tipo_mesclado = ['Nota', 10, 'Nota', 5]
+var objeto = {
+    "nome": "Francisco",
+    "idade":31,
+    "altura":1.78,
+    "situacao":true,
+    "linguage_fav": ['Java', 'JS', 'PHP']
+}
+
+var vazio = null;
+var indefinido = undefined;
+
+//exibindo o conteudo das variaveis
+console.log(num,txt,logico_v,
+    logico_f,lista_mesmo_tipo,lista_tipo_mesclado,objeto,vazio, indefinido)
+
+//escopo das variaveis
